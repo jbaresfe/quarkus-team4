2f5428f74ba60e9a2416938ce842993e31fe7ee8
Rashan Smith <rassmith@new-host.home>
2020-04-07 21:51:01 -0400

    added code to sentimentsResource.java

--- octopus/src/main/java/org/acme/ExampleResource.java
+++ octopus/src/main/java/org/acme/ExampleResource.java
@@ -3,90 +3,15 @@ package org.acme;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import twitter4j.FilterQuery;
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
-import twitter4j.TwitterException;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
-import twitter4j.conf.ConfigurationBuilder;
-
-@Path("/tweets")
+@Path("/hello")
 public class ExampleResource {
 	
-	 public static ConfigurationBuilder getConfigurationBuilder() {
-			ConfigurationBuilder cb = new ConfigurationBuilder();
-			return cb.setDebugEnabled(true)
-			  .setOAuthConsumerKey("********")
-			  .setOAuthConsumerSecret("*******")
-			  .setOAuthAccessToken("*******")
-			  .setOAuthAccessTokenSecret("*******");
-		}
-	 
-	 @GET
-	 @Path("/{topic}")
-	 @Transactional
-	 @Produces(MediaType.TEXT_PLAIN)
-	 public List<String> streamTweets(@PathParam String topic) throws TwitterException {		
-			
-		TwitterStream twitterStream = new TwitterStreamFactory(getConfigurationBuilder().build()).getInstance();
-		List<String> tweets = new ArrayList<String>();
-		
-		StatusListener statusListener = new StatusListener() {
-			
-	        public void onStatus(Status status) {
-	        	String tweetString = "@" + status.getUser().getScreenName() + ":" + status.getText();
-	        	System.out.println(tweetString);
-	        	tweets.add(tweetString);
-	        }
-
-			public void onException(Exception ex) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			public void onScrubGeo(long userId, long upToStatusId) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			public void onStallWarning(StallWarning warning) {
-				// TODO Auto-generated method stub
-				
-			} 
-		};
-			
-		FilterQuery filter = new FilterQuery();
-		String hashtag = "#" + topic;
-	    String keywords[] = {hashtag};
-		filter.track(keywords);
-		twitterStream.addListener(statusListener);
-		twitterStream.filter(filter);
-			
-		return tweets;
-		
-	}
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
--- octopus/src/main/java/org/acme/SentimentResource.java
+++ octopus/src/main/java/org/acme/SentimentResource.java
@@ -10,6 +10,17 @@ import java.util.List;
 import java.util.ArrayList;
 import org.acme.domain.TwitterSentiment;
 
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+
 @Path("/sentiments")
 public class SentimentResource {
 
@@ -34,4 +45,66 @@ public class SentimentResource {
 
         return data;
     }
+    
+    public static ConfigurationBuilder getConfigurationBuilder() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		return cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("*********")
+		  .setOAuthConsumerSecret("********")
+		  .setOAuthAccessToken("*************")
+		  .setOAuthAccessTokenSecret("***********");
+    }
+ 
+	 @GET
+	 @Path("stream/{query}")
+	 @Produces(MediaType.TEXT_PLAIN)
+	 public List<String> streamTwitter(@PathParam("query") String query) throws TwitterException {		
+			
+		TwitterStream twitterStream = new TwitterStreamFactory(getConfigurationBuilder().build()).getInstance();
+		List<String> tweets = new ArrayList<String>();
+		
+		StatusListener statusListener = new StatusListener() {
+			
+	        public void onStatus(Status status) {
+	        	String tweetString = "@" + status.getUser().getScreenName() + ":" + status.getText();
+	        	System.out.println(tweetString);
+	        	tweets.add(tweetString);
+	        }
+	
+			public void onException(Exception ex) {
+				// TODO Auto-generated method stub
+				
+			}
+	
+			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+				// TODO Auto-generated method stub
+				
+			}
+	
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+				// TODO Auto-generated method stub
+				
+			}
+	
+			public void onScrubGeo(long userId, long upToStatusId) {
+				// TODO Auto-generated method stub
+				
+			}
+	
+			public void onStallWarning(StallWarning warning) {
+				// TODO Auto-generated method stub
+				
+			} 
+		};
+			
+		FilterQuery filter = new FilterQuery();
+		String hashtag = "#" + query;
+	    String keywords[] = {hashtag};
+		filter.track(keywords);
+		twitterStream.addListener(statusListener);
+		twitterStream.filter(filter);
+			
+		return tweets;
+		
+	}
 }
