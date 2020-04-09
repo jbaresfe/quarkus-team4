ab6b405313ce8ce1c26bec6fa012552e3e03bd7c
Eric Deandrea <eric.deandrea@gmail.com>
2020-04-09 10:06:17 -0400

    Adding thread safety

--- window-aggregation/src/main/java/com/github/octopus/aggregator/rest/TwitterResource.java
+++ window-aggregation/src/main/java/com/github/octopus/aggregator/rest/TwitterResource.java
@@ -1,130 +1,69 @@
 
 package com.github.octopus.aggregator.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.time.Instant;
-import java.util.List;
-import java.util.ArrayList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import javax.inject.Inject;
-import java.util.concurrent.TimeUnit; 
+import javax.ws.rs.core.Response;
+
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.octopus.aggregator.config.TwitterPropertiesConfig;
 import com.github.octopus.aggregator.config.WindowAggregationPropertiesConfig;
 import com.github.octopus.aggregator.model.TwitterPost;
-
-import twitter4j.FilterQuery;
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
-import twitter4j.TwitterException;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
-import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.HashtagEntity;
+import com.github.octopus.aggregator.util.FunctionalReadWriteLockGuard;
+import com.github.octopus.aggregator.util.TwitterFeedProcessor;
+import com.github.octopus.aggregator.util.TwitterFeedProcessorHolder;
+import io.quarkus.scheduler.Scheduled;
 
 @Path("/twitter")
 public class TwitterResource {
-
+	private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 	private static final Logger LOGGER = LoggerFactory.getLogger("TwitterResource");
 	private final TwitterPropertiesConfig configtwitter;
 	private final WindowAggregationPropertiesConfig config;
 	private final Emitter<TwitterPost> postEmitter;
+	private final List<TwitterFeedProcessorHolder> runningProcessors = new ArrayList<>();
+	private final FunctionalReadWriteLockGuard lockGuard = new FunctionalReadWriteLockGuard(new ReentrantReadWriteLock(true));
 
 	@Inject
 	public TwitterResource(WindowAggregationPropertiesConfig config, TwitterPropertiesConfig configtwitter, @Channel("twitter-posts-out") Emitter<TwitterPost> postEmitter) {
 		this.config = config;
 		this.postEmitter = postEmitter;
 		this.configtwitter = configtwitter;
-	}	
-
-	public ConfigurationBuilder getConfigurationBuilder() {
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		return cb.setDebugEnabled(true).setOAuthConsumerKey(this.configtwitter.consumerKey())
-				.setOAuthConsumerSecret(this.configtwitter.consumerSecret())
-				.setOAuthAccessToken(this.configtwitter.accessToken())
-				.setOAuthAccessTokenSecret(this.configtwitter.accessTokenSecret());
 	}
 
 	@GET
 	@Path("/{query}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<TwitterPost> streamTwitter(@PathParam("query") String query) throws TwitterException, InterruptedException {
-		List<TwitterPost> twitterPosts = new ArrayList<TwitterPost>();
-		
-		TwitterStream twitterStream = new TwitterStreamFactory(getConfigurationBuilder().build()).getInstance();
-		//List<String> tweets = new ArrayList<String>();
+	public Response streamTwitter(@PathParam("query") String query) {
+		TwitterFeedProcessor twitterFeedProcessor = new TwitterFeedProcessor(this.postEmitter, this.configtwitter, query);
+		this.lockGuard.doInWriteLock(() -> this.runningProcessors.add(new TwitterFeedProcessorHolder(twitterFeedProcessor, EXECUTOR_SERVICE.submit(twitterFeedProcessor))));
 
-		StatusListener statusListener = new StatusListener() {
+		return Response.noContent().build();
+	}
 
-			public void onStatus(Status status) {
-				String tweetString = query + "@" + status.getUser().getScreenName() + ":" + status.getCreatedAt().toString() + status.getText() + status.getHashtagEntities();
-				System.out.println(tweetString);
-				//tweets.add(tweetString);
-				String handle = "@" + status.getUser().getScreenName();
-				Instant timestamp = status.getCreatedAt().toInstant();
-				String post = status.getText().toString();
-				List<String> hashtagList = new ArrayList<String>(); 
-				HashtagEntity[] tags = status.getHashtagEntities();
-				
-				for (HashtagEntity tag : tags) {
-	                String hash = tag.getText();
-	                hashtagList.add(hash);
-	            } 
-				
-				TwitterPost somePost = new TwitterPost(query, handle, timestamp, post, hashtagList);
-				
-				twitterPosts.add(somePost);
-				
-			}
+	@Scheduled(every = "1m")
+	public void stopThreads() {
+		this.lockGuard.doInWriteLock(() -> {
+			this.runningProcessors.forEach(processor -> {
+				processor.getTwitterFeedProcessor().stop();
+				processor.getProcessorFuture().cancel(false);
+			});
 
-			public void onException(Exception ex) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public void onScrubGeo(long userId, long upToStatusId) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public void onStallWarning(StallWarning warning) {
-				// TODO Auto-generated method stub
-
-			}
-		};
-
-		FilterQuery filter = new FilterQuery();
-		String hashtag = "#" + query;
-		String keywords[] = { hashtag };
-		filter.track(keywords);
-		twitterStream.addListener(statusListener);
-		twitterStream.filter(filter);
-		Thread.sleep(30000);
-		twitterStream.shutdown();
-
-		List<TwitterPost> posts = twitterPosts;
-		posts.forEach(this.postEmitter::send);
-
-		LOGGER.info("After posting into Kafka");
-
-		return posts;
+			this.runningProcessors.clear();
+		});
 	}
 }
--- window-aggregation/src/main/java/com/github/octopus/aggregator/util/TwitterFeedProcessor.java
+++ window-aggregation/src/main/java/com/github/octopus/aggregator/util/TwitterFeedProcessor.java
@@ -1,88 +1,81 @@
 package com.github.octopus.aggregator.util;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.octopus.aggregator.config.TwitterPropertiesConfig;
 import com.github.octopus.aggregator.model.TwitterPost;
-
-import org.jboss.resteasy.spi.RunnableWithException;
-
-import io.reactivex.Emitter;
+import twitter4j.FilterQuery;
+import twitter4j.HashtagEntity;
+import twitter4j.Status;
+import twitter4j.StatusAdapter;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterFeedProcessor implements Runnable {
-    private final Emitter<TwitterPost> postEmitter;
-    
-    @Override
-    public void run() {
-		List<TwitterPost> twitterPosts = new ArrayList<TwitterPost>();
+	private static final Logger LOGGER = LoggerFactory.getLogger(TwitterFeedProcessor.class);
+	private final TwitterPropertiesConfig configtwitter;
+	private final Emitter<TwitterPost> postEmitter;
+	private final TwitterStream twitterStream;
+	private final String query;
+	private Instant startTime;
 
-		StatusListener statusListener = new StatusListener() {
+	public TwitterFeedProcessor(Emitter<TwitterPost> postEmitter, TwitterPropertiesConfig configtwitter, String query)
+	{
+		this.postEmitter = postEmitter;
+		this.query = query;
+		this.configtwitter = configtwitter;
+		this.twitterStream = new TwitterStreamFactory(getConfigurationBuilder().build()).getInstance();
+	}
 
+	private ConfigurationBuilder getConfigurationBuilder() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		return cb.setDebugEnabled(true).setOAuthConsumerKey(this.configtwitter.consumerKey())
+			.setOAuthConsumerSecret(this.configtwitter.consumerSecret())
+			.setOAuthAccessToken(this.configtwitter.accessToken())
+			.setOAuthAccessTokenSecret(this.configtwitter.accessTokenSecret());
+	}
+
+	public void stop() {
+		this.twitterStream.clearListeners();
+	}
+
+	@Override
+	public void run() {
+		this.startTime = Instant.now();
+		StatusListener statusListener = new StatusAdapter() {
+			@Override
 			public void onStatus(Status status) {
-				String tweetString = query + "@" + status.getUser().getScreenName() + ":" + status.getCreatedAt().toString() + status.getText() + status.getHashtagEntities();
-				System.out.println(tweetString);
-				//tweets.add(tweetString);
-				String handle = "@" + status.getUser().getScreenName();
+				String tweetString = TwitterFeedProcessor.this.query + "@" + status.getUser().getScreenName() + ":" + status.getCreatedAt().toString() + status.getText() + status.getHashtagEntities();
+				TwitterFeedProcessor.LOGGER.info("tweet string = {}", tweetString);
+				String handle = String.format("@%s", status.getUser().getScreenName());
 				Instant timestamp = status.getCreatedAt().toInstant();
-				String post = status.getText().toString();
-				List<String> hashtagList = new ArrayList<String>(); 
+				String post = status.getText();
 				HashtagEntity[] tags = status.getHashtagEntities();
-				
-				for (HashtagEntity tag : tags) {
-	                String hash = tag.getText();
-	                hashtagList.add(hash);
-	            } 
-				
-				TwitterPost somePost = new TwitterPost(query, handle, timestamp, post, hashtagList);
-				
-				twitterPosts.add(somePost);
-				
-			}
 
-			public void onException(Exception ex) {
-				// TODO Auto-generated method stub
+				List<String> hashtagList = new ArrayList<>(
+					Arrays.stream(tags)
+						.map(HashtagEntity::getText)
+						.collect(Collectors.toList())
+				);
 
-			}
-
-			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public void onScrubGeo(long userId, long upToStatusId) {
-				// TODO Auto-generated method stub
-
-			}
-
-			public void onStallWarning(StallWarning warning) {
-				// TODO Auto-generated method stub
-
+				TwitterPost twitterPost = new TwitterPost(TwitterFeedProcessor.this.query, handle, timestamp, post, hashtagList);
+				TwitterFeedProcessor.this.postEmitter.send(twitterPost);
+				TwitterFeedProcessor.LOGGER.info("Sent post to kafka: {}", twitterPost);
 			}
 		};
 
-		FilterQuery filter = new FilterQuery();
-		String hashtag = "#" + query;
-		String keywords[] = { hashtag };
-		filter.track(keywords);
-		twitterStream.addListener(statusListener);
-		twitterStream.filter(filter);
-		Thread.sleep(30000);
-		twitterStream.shutdown();
-
-		List<TwitterPost> posts = twitterPosts;
-		posts.forEach(this.postEmitter::send);
-
-		LOGGER.info("After posting into Kafka");
-
-		return posts;
+		this.twitterStream.addListener(statusListener);
+		this.twitterStream.filter(new FilterQuery(String.format("#%s", this.query)));
+//		Thread.sleep(30000);
 	}
-
-    public TwitterFeedProcessor(Emitter<TwitterPost> postEmitter, TwitterStream twitterStream, String query)
-    {
-        this.postEmitter = postEmitter;
-        this.twitterStream = twitterStream;
-        this.query = query;
-    }
 }
